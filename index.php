f41f8574165438862921d08aa5c018bf9cd825d2
dameat <dameat@1cb5bcb2-e76f-4972-bee4-7e557178d5bf>
2011-03-21 23:23:20 +0000

    made lots of changes (for the better?)
    git-svn-id: file:///var/lib/svn/blocharts@79 1cb5bcb2-e76f-4972-bee4-7e557178d5bf

--- index.php
+++ index.php
@@ -1,33 +1,38 @@
 <?php
-$workingdir = '/home/dameat/public_html/pondscum/blocharts/';
-$lilydir = $workingdir . ($_REQUEST['dir'] ? $_REQUEST['dir'] : 'blo');
-$keys = array('Eb'=>'a', 'Bb'=>'d', 'C'=>'c', 'F'=>'g');
-$clefs = array('treble', 'bass', 'alto', 'tenor');
-$layouts = array('letter'=>'"letter"', 'lyre'=>'"a5" \'landscape');
-$octaves = array('+2'=> "''", '+1'=>"'", '0'=>'', "-1"=>',','-2'=>',,');
-$instruments = array('bass'=>'tuba', 'melody'=>'trumpet', 'tenor'=>'trombone', 'pahs'=>'trombone', 'riffTwo'=>'clarinet', 'harmony'=>'clarinet', 'chordLo'=>'trombone', 'chordMid'=>'baritone sax', 'bari'=>'baritone sax');
+include('pondscum.php');
 
-if($_GET['file']) {
+$workingdir = '/home/dameat/public_html/pondscum/blocharts/';
+$dir = isset($_REQUEST['dir']) ?  $_REQUEST['dir'] : "blo";
+$lilydir = $workingdir . $dir;
+
+if(isset($_GET['file'])) {
 	$file = $_GET['file'];
 	if (substr($file, 0, 1) == "/" || strpos($file, '..') !== false) {
 		error('Invalid filename');
 	}
 	$lily = processFile($file, $_REQUEST['dir']);
-	$lily = buildLayout($lily);
-	createOutput($lily);
-	
+	if ($lily) { 
+		$lily['outputoptions'] = $_GET;
+		$lily = buildLayout($lily);
+		if ($lily) { 
+			printHTTPHeader($lily, $_REQUEST['part']);
+			print createOutput($lily);
+		}
+	}	
 	#if($_GET['part'] == 'midi') { returnMidi($lily); }
-	if (strpos($score, 'changes')) {
+	#if (strpos($score, 'changes')) {
 		#$changes = '\changes';
-	}
+	#}
 	#createOutput($file, $score);
 } else {
 	header('Content-Type: text/html; charset=utf-8'); 
 	$dirh = opendir($lilydir);
 	while (($file = readdir($dirh)) !== false) {
-		$lilies[] = processFile($file, $_REQUEST['dir']);
+		$lilies[] = processFile($file, $dir);
 	}
 	usort($lilies, 'lilysort');
+	$index = "";
+	$forms = "";
 	foreach ($lilies as $lily) { 
 		if ($lily) { 
 			$index .= "<li><a href='#$lily[file]'>$lily[title]</a>";
@@ -37,195 +42,4 @@ if($_GET['file']) {
 	print "$index<hr>$forms";
 }
 
-function createOutput($lily) {
-	$part = $_GET['part'];
-	$contents = $lily['source'].$lily['layout'];
-	$filename = $lily[file];
-	$outfile = str_replace(' ', '_', $lily['title']). "-$key-$part";
-	if ($part == 'source') {
-		header('Content-Type: text/html; charset=utf-8'); 
-		print "<pre>$contents</pre>";
-		exit;
-	}
-	if($part != 'midi') { $part = 'pdf'; }
-	$out = fopen("/tmp/$filename", 'w');
-	fwrite($out,$contents);
-	fclose($out);
-	exec("lilypond -o '/tmp/$filename' '/tmp/$filename' 2>&1", $error);
-	if ($_GET['debug']) { print "<pre>$contents"; print "<hr>".$error."<hr>".join('<br>', $output); exit; }
-	if (!file_exists("/tmp/$filename.$part") || filesize("/tmp/$filename.$part") == 0) { 
-		error("Lilypond failed. Here is the output:<hr> ".implode("<br>", $error));
-	}
-	header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-	if ($part == 'midi') { 
-		header('Content-type: audio/midi');
-		header('Content-Disposition: attachment; filename="'.$outfile.'.mid"');
-		print file_get_contents("/tmp/$filename.midi");
-		unlink("/tmp/$filename.midi");
-	} else { 
-		if (!file_exists("/tmp/$filename.pdf")) { 
-			error("Lilypond failed: $error");
-		}
-		header('Content-type: application/pdf');
-		header('Content-Disposition: attachment; filename="'.$lily[filename].'.pdf"');
-		print file_get_contents("/tmp/$filename.pdf");
-		unlink("/tmp/$filename.pdf");
-		unlink("/tmp/$filename.ps");
-		unlink("/tmp/$filename");
-	}	
-}
 
-function processFile($file, $dir='blo') {
-	global $lilydir;
-	$lily = array();
-	if (!file_exists("$lilydir/$file")) { 
-		error("File $lilydir/$file does not exist");
-	}
-	if (preg_match('/.ly$/', $file)){
-		$score = file_get_contents("$lilydir/$file");
-		preg_match_all('/%Part: (\w+)/i', $score, $parts);
-		preg_match('/title ?=[^"]*"([^"]+)"/', $score, $title);
-		preg_match('/tempo(.*)$/m', $score, $tempo);
-		$lily['title'] = $title[1];
-		$lily['parts'] = $parts[1];
-		$lily['tempo'] = $tempo[1] ? $tempo[1] : ' 4 = 100';
-		$lily['file'] = $file;
-		$lily['source'] = preg_replace('/\%layout.*/si', '', $score);
-		$lily['changes'] = array_search('changes', $lily['parts']) ? 1 : 0;
-		$lily['words'] = array_search('words', $lily['parts']) ? 1 : 0;
-		$lily['dir'] = $dir;
-	}
-	return $lily;
-}
-
-function printFileSelect($lily) {	
-	global $keys, $clefs, $octaves, $layouts;
-	$output .= "<a name='$lily[file]'><div style='position: relative;'><form>
-		<input name='file' type='hidden' value='$lily[file]'>
-		<input name='dir' type='hidden' value='$lily[dir]'>
-		<b>".html_encode($lily['title'])."</b>";
-	foreach ($lily['parts'] as $part) { 
-		$partselect .= "<option value='$part'>".ucwords($part)."</option>";
-	}
-	foreach (array_keys($keys) as $key) { $keyselect .= "<option value='$key'>".ucwords($key)."</option>"; }
-	foreach ($clefs as $clef) { $clefselect .= "<option value='$clef'>".ucwords($clef)."</option>"; }
-	foreach (array_keys($octaves) as $octave) { $default = $octave == 0 ? "selected='selected'" : ''; $octaveselect .= "<option $default value='$octave'>".ucwords($octave)."</option>"; }
-	foreach (array_keys($layouts) as $layout) { $layoutselect .= "<option value='$layout'>".ucwords($layout)."</option>"; }
-
-	$output.= "<br><a href='?file=$lily[file]&dir=$lily[dir]&part=midi'>play midi</a>";
-	$output.= " - <a href='?file=$lily[file]&dir=$lily[dir]&part=score'>score </a>";
-	$output.= " - <a href='?file=$lily[file]&dir=$lily[dir]&part=source'>source</a>";
-	$output.= "<div class='options' style='position: relative;'> <div class='left'>
-				<div class='part'>Part: <select name='part'>$partselect</select></div>
-				<div class='key'>Key: <select name='key'>$keyselect</select></div>
-				<div class='clef'>Clef: <select name='clef'>$clefselect</select></div>
-			</div>
-			<div class='right' style='position: absolute; left: 180px; top: 0px;'>
-				<div class='octave'>Octave: <select name='octave'>$octaveselect</select></div>
-				<div class='layout'>Layout: <select name='page'>$layoutselect</select></div>
-		";
-	if ($lily['words']) {
-		$output.= "<div class='lyrics'>Include Lyrics: <input type='checkbox' name='words'></div>";
-	}
-	$output.= " 	<br>Debug <input type='checkbox' name='debug' value='1'><br>
-				</div>
-				<input style='margin: auto; text=align: center;' type='submit' value='Download PDF'>
-			</form></div></div>
-			";
-	return $output;
-}
-
-function buildLayout($lily) {
-	global $keys, $layouts, $octaves, $instruments;
-	$key = $_GET['key'];
-	$clef = $_GET['clef'];
-	$page = $_GET['page'];
-	$part = $_GET['part'];
-	$showwords = $_GET['words'];
-	$octave = stripslashes($_GET['octave']);
-	if ($part == 'score' || $part == 'source') { $page = 'letter'; }
-	$layout = "%%Generated layout";
-	if ($lily['changes'] && $page != 'lyre') { $changes = "\n\t\t\\new ChordNames { \\set chordChanges = ##t \\changes }"; }
-	if ($lily['words']) { $words = " \n\t\words"; }
-	if ($page) {  
-		$layout .= "\n#(set-default-paper-size ".$layouts[$page].')';
-	}
-	if ($part == 'score'  || $part == 'source' || $part == 'midi') {
-		$parts = $lily['parts'];
-		$layout .= "\n\\book {\n\t\\score { <<\n\t\t\\tempo ".$lily['tempo']. ' ';
-		if ($part != 'midi') { $layout .= $changes; }
-		foreach ($parts as $lilypart) { 
-			if ($lilypart == 'changes' || $lilypart == 'words') { continue; }
-			$instrument = $instruments[$lilypart] ? $instruments[$lilypart] : 'alto sax';
-			$clef = $lilypart == 'bass' ? 'bass' : 'treble';
-			$layout .="\n\t\t";
-			if ($part == 'midi') { $layout .= '\unfoldRepeats '; }
-			$layout .= "\\new Staff {  \\set Staff.midiInstrument = #\"$instrument\" \\clef $clef";
-			$layout .= "\n\t\t\t\\$lilypart\n\t\t}";
-		}
-		$layout .= "\n\t>> ";
-		if ($part == 'midi') { $layout .= "\n\t\\midi { } "; }
-		$layout .= "} $words \n}";
-	} else { 
-		$octave = getOctave($key, $part, $clef, $octaves[$octave]);
-		$poet = "$key ".ucwords($part);
-		$layout .="
-		\\book { 
-			\\header{ poet = \"$poet\" }
-			\\score { <<
-				$changes
-				\\new Staff { \\clef $clef \\transpose c ".$keys[$key].$octave." 
-				\\$part
-				}
-			>> }";
-		if ($showwords) {
-			$layout .= $words;
-		} 
-		$layout .= "\n} ";
-	}
-	$lily['layout'] = $layout;
-	$lily['filename'] = $lily['title']."-$key-".ucwords($part);
-	return $lily;	
-}
-
-function getOctave($key, $part, $clef, $octave) {
-	if ($key == 'a' || $key == 'g') { 
-		if (strpos($octave, "'") !== false) { 
-			$octave = substr_replace($octave, '', -1, 1);
-		} else {
-			$octave.=",";
-		}
-	}
-	if ($clef == 'bass') { 
-		if (strpos($octave, "'") !== false) { 
-			$octave = substr_replace($octave, '', -1, 1);
-		} else {
-			$octave.=","; 
-		}
-	} else if ($part == 'bass' && $clef == 'treble') {
-		if (strpos($octave, ",") !== false) { 
-			$octave = substr_replace($octave, '', -1, 1);
-		} else {
-			$octave.="'";
-		}
-	}
-	return $octave;
-}
-
-function html_encode($var) {
-		return htmlentities($var, ENT_QUOTES, 'UTF-8') ;
-}
-
-function error($string) {
-	print "<b>An error occured:</b> $string";
-	exit;
-}
-
-function lilysort($a, $b) { 
-	if ($a['title'] && $b['title']) {
-		return strcasecmp($a['title'], $b['title']);
-	} else {
-		return strcasecmp($a['file'], $b['file']);
-	}
-}
---
+++ pondscum.php
@@ -0,0 +1,298 @@
+<?php
+
+$keys = array('Eb'=>'a', 'Bb'=>'d', 'C'=>'c', 'F'=>'g');
+$clefs = array('treble', 'bass', 'alto', 'tenor');
+$layouts = array('letter'=>'"letter"', 'lyre'=>'"b6" \'landscape');
+$octaves = array('+2'=> "''", '+1'=>"'", '0'=>'', "-1"=>',','-2'=>',,');
+$instruments = array('bass'=>'tuba', 'melody'=>'trumpet', 'tenor'=>'trombone', 'pahs'=>'trombone', 'riffTwo'=>'clarinet', 'harmony'=>'clarinet', 'chordLo'=>'trombone', 'chordMid'=>'baritone sax', 'bari'=>'baritone sax');
+
+function printHTTPHeader($lily, $part) {
+	global $_REQUEST;
+	if ($part == 'source' || isset($_REQUEST['debug'])) {
+		header('Content-Type: text/html; charset=utf-8'); 
+		return;
+	} 
+	$filename = $lily['filename'];
+	header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+ 
+	if ($part =='midi') {
+		header('Content-type: audio/midi');
+		header('Content-Disposition: attachment; filename="'.$filename.'.mid"');
+	} else {
+		header('Content-type: application/pdf');
+		header('Content-Disposition: attachment; filename="'.$filename.'.pdf"');
+	}
+}
+
+function createOutput($lily) {
+	$part = $lily['outputoptions']['part'];
+	$contents = $lily['source'].$lily['layout'];
+	$filename = $lily['file'];
+	$output = "";
+	if ($part == 'source') {
+		$output =  "<pre>$contents</pre>";
+	} else {
+		if($part != 'midi') { $part = 'pdf'; }
+		$out = fopen("/tmp/$filename", 'w');
+		fwrite($out,$contents);
+		fclose($out);
+		exec("lilypond -o '/tmp/$filename' '/tmp/$filename' 2>&1", $error);
+		if (isset($lily['outputoptions']['debug'])) { 
+			$output = "<pre>$contents<hr>".implode('<br>', $error)."<hr>".print_r($output, 1);
+		} else {
+			if (!file_exists("/tmp/$filename.$part") || filesize("/tmp/$filename.$part") == 0) { 
+				error("Lilypond failed. Here is the output:<hr> ".implode("<br>", $error));
+				return;
+			}
+			if ($part == 'midi') { 
+				$output = file_get_contents("/tmp/$filename.midi");
+				unlink("/tmp/$filename.midi");
+			} else { 
+				if (!file_exists("/tmp/$filename.pdf")) { 
+					error("Lilypond failed: $error");
+					return;
+				}
+				$output = file_get_contents("/tmp/$filename.pdf");
+				unlink("/tmp/$filename.pdf");
+				unlink("/tmp/$filename.ps");
+				unlink("/tmp/$filename");
+			}
+		}	
+	}
+	return $output;
+}
+
+function processFile($file, $dir='blo') {
+	global $lilydir;
+	$lily = array();
+	if (!file_exists("$lilydir/$file")) { 
+		error("File $lilydir/$file does not exist");
+		return;
+	}
+	if (preg_match('/.ly$/', $file)){
+		$score = file_get_contents("$lilydir/$file");
+		preg_match_all('/%Part: (\w+)/i', $score, $parts);
+		preg_match('/title ?=[^"]*"([^"]+)"/', $score, $title);
+		preg_match('/tempo(.*)$/m', $score, $tempo);
+		$lily['title'] = $title[1];
+		$lily['parts'] = $parts[1];
+		$lily['tempo'] = isset($tempo[1]) ? $tempo[1] : ' 4 = 100';
+		$lily['file'] = $file;
+		$lily['source'] = preg_replace('/\%layout.*/si', '', $score);
+		$lily['changes'] = array_search('changes', $lily['parts']) ? 1 : 0;
+		$lily['words'] = (array_search('words', $lily['parts']) && ! strstr($score, 'words = \markup { }')) ? 1 : 0;
+		$lily['dir'] = $dir;
+	}
+	return $lily;
+}
+
+function printFileSelect($lily) {	
+	global $keys, $clefs, $octaves, $layouts;
+	$output = "<a name='$lily[file]'><div style='position: relative;'><form>
+		<input name='file' type='hidden' value='$lily[file]'>
+		<input name='dir' type='hidden' value='$lily[dir]'>
+		<b>".html_encode($lily['title'])."</b>";
+	$partselect = "";
+	$keyselect = "";
+	$octaveselect = "";
+	$clefselect = "";
+	$layoutselect = "";
+	foreach ($lily['parts'] as $part) { 
+		$partselect .= "<option value='$part'>".ucwords($part)."</option>";
+	}
+	foreach (array_keys($keys) as $key) { $keyselect .= "<option value='$key'>".ucwords($key)."</option>"; }
+	foreach ($clefs as $clef) { $clefselect .= "<option value='$clef'>".ucwords($clef)."</option>"; }
+	foreach (array_keys($octaves) as $octave) { $default = $octave == 0 ? "selected='selected'" : ''; $octaveselect .= "<option $default value='$octave'>".ucwords($octave)."</option>"; }
+	foreach (array_keys($layouts) as $layout) { $layoutselect .= "<option value='$layout'>".ucwords($layout)."</option>"; }
+
+	$output.= "<br><a href='?file=$lily[file]&dir=$lily[dir]&part=midi'>play midi</a>";
+	$output.= " - <a href='?file=$lily[file]&dir=$lily[dir]&part=score'>score </a>";
+	$output.= " - <a href='?file=$lily[file]&dir=$lily[dir]&part=source'>source</a>";
+	$output.= "<div class='options' style='position: relative;'> <div class='left'>
+				<div class='part'>Part: <select name='part'>$partselect</select></div>
+				<div class='key'>Key: <select name='key'>$keyselect</select></div>
+				<div class='clef'>Clef: <select name='clef'>$clefselect</select></div>
+			</div>
+			<div class='right' style='position: absolute; left: 180px; top: 0px;'>
+				<div class='octave'>Octave: <select name='octave'>$octaveselect</select></div>
+				<div class='layout'>Layout: <select name='page'>$layoutselect</select></div>
+		";
+	if ($lily['words']) {
+		$output.= "<div class='lyrics'>Include Lyrics: <input type='checkbox' name='words'></div>";
+	}
+	$output.= "<div class='naturalize'>Naturalize Accidentals: <input type='checkbox' name='naturalize' checked='1'></div>";
+	$output.= " 	<br>Debug <input type='checkbox' name='debug' value='1'><br>
+				</div>
+				<input style='margin: auto; text=align: center;' type='submit' value='Download PDF'>
+			</form></div></div>
+			";
+	return $output;
+}
+
+function buildLayout($lily) {
+	global $keys, $layouts, $octaves, $instruments, $naturalize_function;
+	$part = $lily['outputoptions']['part'];
+	$key = $lily['outputoptions']['key'];
+	if (! $key) { $key = 'c'; }
+	$showwords = isset($lily['outputoptions']['words']) ? $lily['outputoptions']['words'] : "";
+	if ($part == 'score' || $part == 'source') { $page = 'letter'; }
+	$layout = "%%Generated layout";
+	$layout .= $naturalize_function;
+	$changes = "";
+	if ($lily['changes']) { $changes = "\n\t\t\\transpose c ".$keys[$key]." \\new ChordNames { \\set chordChanges = ##t \\changes }"; }
+	$words = "";
+	if ($lily['words']) { $words = " \n\t\words"; }
+	if ($part == 'score'  || $part == 'source' || $part == 'midi') {
+		$parts = $lily['parts'];
+		$layout .= "\n#(set-default-paper-size ".$layouts[$page].')';
+		$layout .= "\n\\book {\n\t\\score { <<\n\t\t\\tempo ".$lily['tempo']. ' ';
+		if ($part != 'midi') { $layout .= $changes; }
+		foreach ($parts as $lilypart) { 
+			if ($lilypart == 'changes' || $lilypart == 'words') { continue; }
+			$instrument = isset($instruments[$lilypart]) ? $instruments[$lilypart] : 'alto sax';
+			$clef = $lilypart == 'bass' ? 'bass' : 'treble';
+			$layout .="\n\t\t";
+			if ($part == 'midi') { $layout .= '\unfoldRepeats '; }
+			$layout .= "\\new Staff \\with { \\consists \"Volta_engraver\" } {  \\set Staff.midiInstrument = #\"$instrument\" \\clef $clef";
+			$layout .= "\n\t\t\t\\$lilypart\n\t\t}";
+		}
+		$layout .= "\n\t>> \\layout { \\context { \\Score \\remove \"Volta_engraver\" } } ";
+		if ($part == 'midi') { $layout .= "\n\t\\midi { } "; $words = "";}
+		$layout .= "} $words \n}";
+	} else { 
+		$page = $lily['outputoptions']['page'];
+		$clef = $lily['outputoptions']['clef'];
+		$octave = stripslashes($lily['outputoptions']['octave']);
+		$octave = getOctave($key, $part, $clef, $octaves[$octave]);
+		$poet = "$key ".ucwords($part);
+		$staffspacing = "";
+		if($page == 'lyre') { 
+			$layout .= "\n#(set-global-staff-size 15)\n"; 
+			$staffspacing = "\\override Staff.VerticalAxisGroup #'minimum-Y-extent = #'(-1 . 1)";
+			$changes = "";
+		} else {
+			$layout .= "\n#(set-default-paper-size ".$layouts[$page].')';
+		}
+		$naturalize = "";
+		if ($lily['outputoptions']['naturalize']) {
+			$naturalize = "\\naturalizeMusic ";
+		}
+		$layout .="
+		\\book { 
+			\\header{ poet = \"$poet\" }
+			\\score { <<
+				$changes
+				\\new Staff \\with { \\consists \"Volta_engraver\" } { 
+					$staffspacing	
+					\\clef $clef 
+				$naturalize \\transpose c ".$keys[$key].$octave."
+				\\$part
+				}
+			
+			>> 
+			\\layout { \\context { \\Score \\remove \"Volta_engraver\" } } 
+	
+			}
+		
+		";
+		if ($showwords) {
+			$layout .= $words;
+		} 
+		$layout .= "\n} ";
+	}
+	$lily['layout'] = $layout;
+	$keyname = "";
+	if (isset($key)) { 
+		$keyname = "-$key";
+	}
+	$lily['filename'] = $lily['title']."$keyname-".ucwords($part);
+	return $lily;	
+}
+
+function getOctave($key, $part, $clef, $octave) {
+	if ($key == 'a' || $key == 'g') { 
+		if (strpos($octave, "'") !== false) { 
+			$octave = substr_replace($octave, '', -1, 1);
+		} else {
+			$octave.=",";
+		}
+	}
+	if ($clef == 'bass') { 
+		if (strpos($octave, "'") !== false) { 
+			$octave = substr_replace($octave, '', -1, 1);
+		} else {
+			$octave.=","; 
+		}
+	} else if ($part == 'bass' && $clef == 'treble') {
+		if (strpos($octave, ",") !== false) { 
+			$octave = substr_replace($octave, '', -1, 1);
+		} else {
+			$octave.="'";
+		}
+	}
+	return $octave;
+}
+
+function html_encode($var) {
+		return htmlentities($var, ENT_QUOTES, 'UTF-8') ;
+}
+
+function error($string) {
+	print "<b>An error occured:</b> $string";
+}
+
+function lilysort($a, $b) { 
+	if (isset($a['title']) && isset($b['title'])) {
+		return strcasecmp($a['title'], $b['title']);
+	} else if(isset($a['file']) && isset($b['file'])) {
+		return strcasecmp($a['file'], $b['file']);
+	}
+}
+
+$naturalize_function = "
+#(define (naturalize-pitch p)
+  (let ((o (ly:pitch-octave p))
+        (a (* 4 (ly:pitch-alteration p)))
+        ;; alteration, a, in quarter tone steps,
+        ;; for historical reasons
+        (n (ly:pitch-notename p)))
+    (cond
+     ((and (> a 1) (or (eq? n 6) (eq? n 2)))
+      (set! a (- a 2))
+      (set! n (+ n 1)))
+     ((and (< a -1) (or (eq? n 0) (eq? n 3)))
+      (set! a (+ a 2))
+      (set! n (- n 1))))
+    (cond
+     ((> a 2) (set! a (- a 4)) (set! n (+ n 1)))
+     ((< a -2) (set! a (+ a 4)) (set! n (- n 1))))
+    (if (< n 0) (begin (set! o (- o 1)) (set! n (+ n 7))))
+    (if (> n 6) (begin (set! o (+ o 1)) (set! n (- n 7))))
+    (ly:make-pitch o n (/ a 4))))
+
+#(define (naturalize music)
+  (let ((es (ly:music-property music 'elements))
+        (e (ly:music-property music 'element))
+        (p (ly:music-property music 'pitch)))
+    (if (pair? es)
+       (ly:music-set-property!
+         music 'elements
+         (map (lambda (x) (naturalize x)) es)))
+    (if (ly:music? e)
+       (ly:music-set-property!
+         music 'element
+         (naturalize e)))
+    (if (ly:pitch? p)
+       (begin
+         (set! p (naturalize-pitch p))
+         (ly:music-set-property! music 'pitch p)))
+    music))
+
+naturalizeMusic =
+#(define-music-function (parser location m)
+  (ly:music?)
+  (naturalize m))
+";
+
+?>
